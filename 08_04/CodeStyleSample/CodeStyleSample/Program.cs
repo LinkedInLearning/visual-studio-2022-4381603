ffbff43f401384b097b56ab9e2c9333f19898598
DESKTOP-S959DPI\macni <mac.nishimura@gmail.com>
2023-02-20 20:39:26 +0900

    チャプター08のサンプルコードを追加

---
+++ 08_04/CodeStyleSample/CodeStyleSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeStyleSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface IPerson
+    {
+        string Name { get; set; }
+
+        int Age { get; set; }
+    }
+
+    public class Customer : IPerson
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+    }
+
+}
